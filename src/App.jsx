1f022aa1a0199af36c5061cfecf231f85d50e046
zubaidi-1 <abdallazubaidi7@gmail.com>
2025-05-21 14:29:25 -0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [parameter, setParameter] = useState("");
 
+  const handleChange = function (e) {
+    setParameter(e.target.value);
+  };
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+
+    setCount((prev) => prev + 1);
+    if (parameter === "") {
+      alert("Insert Something!");
+      return;
+    }
+
+    if (parameter.toLowerCase() === "work better") {
+      window.open(
+        "https://www.youtube.com/watch?v=Ct6BUPvE2sM&list=RDQM0J7n_uvu778&start_radio=1&ab_channel=UltraRecords"
+      );
+      return;
+    }
+    window.open(
+      `https://www.detroitaxle.com/part/part/rack-and-pinion/power-steering-rack-and-pinion/${parameter}/`
+    );
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Search parts</h1>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} />
+        <button type="submit">Search!</button>
+      </form>
+      <h2>Times you searched for parts are only {count} ! Do better!</h2>
+      <h3>There is an easter egg hidden Somewhere </h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
